31192f72a7e02f9378e4085bf2373387e07cab73
gbrcarvalho <gccomputacao@gmail.com>
2026-06-15 05:34:35 -0300

    adicionar modulo de configuracao do timer 1 e integrar no teste do projeto, feito por davi reis e davi

--- src/main.asm
+++ src/main.asm
@@ -1,5 +1,19 @@
+; ---------------------------------------------
+;  PROJETO DE CONTROLE DE VENTILADOR POR TEMPERATURA COM ATMEGA328P
+;
+;  Disciplina: Programação de Software Básico - MATA49
+;  Turma: 2
+;  Equipe:
+;  Gabriel Silva Carvalho
+;  Leonardo de Andrade Porto
+;  Davi Reis
+;  Davi
+;  Clock do CPU: 16.000.000 Hz
+; --------------------------------------------
+
 .include "m328Pdef.inc"
 .include "defs_display.inc"
+.include "defs_timer.inc"
 
 ; teste
 .def DECIMO = R24
@@ -9,7 +23,16 @@
 
 RJMP Inicio
 
+; =========================================================
+; VETOR DE INTERRUPÇÃO DO TIMER1 COMPARE MATCH A
+; =========================================================
+
+.ORG OC1Aaddr
+    RJMP ISR_Timer1
+
+.ORG 0x0034         ; endereço após o último vetor do 328P
 .include "display.asm"
+.include "timer.asm"
 
 Inicio:
     ; inicializa a pilha
@@ -18,10 +41,15 @@ Inicio:
     LDI AUX, LOW(RAMEND)
     OUT SPL, AUX
 
+    LDI AUX, 0x01
+    OUT DDRD, AUX
+    OUT PORTD, AUX
+
     LDI DECIMO, 0 ; valor que será incrementado continuamente - teste
     LDI UNIDADE, 0 ; valor que será incrementado continuamente - teste
     LDI DEZENA, 0 ; valor que será incrementado continuamente - teste
     RCALL Inicializa_Display
+    RCALL Timer1_Init
 
 Main:
     MOV DIG1, DEZENA ;; teste
@@ -29,6 +57,10 @@ Main:
     MOV DIG3, DECIMO ;; teste
 
     RCALL Exibe_Display
+
+    LDS AUX, FLAG_DHT
+    CPI AUX, 1
+    BREQ inverte_portd0
 
 ; teste, codigo fica incrementando o valor
     INC DECIMO
@@ -57,3 +89,12 @@ zera:
     LDI DEZENA, 0
     RJMP Main
 ; apagar esse codigo de teste
+
+inverte_portd0:
+    IN   AUX, PORTD
+    LDI  R28, 1
+    EOR  AUX, R28
+    OUT  PORTD, AUX
+    LDI  AUX, 0
+    STS  FLAG_DHT, AUX
+    RJMP Main
---
+++ src/timer.asm
@@ -0,0 +1,110 @@
+; =========================================================
+; VARIÁVEIS NA SRAM
+; =========================================================
+
+.DSEG 
+
+CONTADOR_SEGUNDOS: .BYTE 1 ; armazena os segundos passados
+FLAG_DHT:          .BYTE 1 ; flag indicando o momento de ler o DHT
+
+.CSEG
+
+; =========================================================
+; ROTINA DE INICIALIZAÇÃO DO TIMER1
+; =========================================================
+
+Timer1_Init:
+
+    LDI AUX, 0x00
+    STS CONTADOR_SEGUNDOS, AUX
+    STS FLAG_DHT, AUX
+
+    ; Modo normal para TCCR1A
+
+    LDI AUX, 0x00
+    STS TCCR1A, AUX
+
+    ; WGM12 = 1 (CTC)
+    ; CS12 = 1 e CS10 = 1 (Prescaler 1024)
+
+    LDI AUX, 0b00001101
+    STS TCCR1B, AUX
+
+    ; Valor de comparação
+    ; 15624 = 1 segundo com clock de 16 MHz
+
+    LDI AUX, HIGH(15624)
+    STS OCR1AH, AUX
+
+    LDI AUX, LOW(15624)
+    STS OCR1AL, AUX
+
+    ; Habilita interrupção Compare Match A
+
+    LDI AUX, 0b00000010
+    STS TIMSK1, AUX
+
+    ; Habilita interrupções globais
+
+    SEI
+
+    RET
+
+
+; =========================================================
+; ISR: INTERRUPÇÃO DO TIMER1
+; Executa a cada 1 segundo
+; =========================================================
+
+ISR_Timer1:
+
+    ; -----------------------------------------
+    ; Salva contexto
+    ; -----------------------------------------
+
+    PUSH AUX
+
+    IN   AUX, SREG
+    PUSH AUX
+
+    ; -----------------------------------------
+    ; Incrementa contador de segundos
+    ; -----------------------------------------
+
+    LDS  AUX, CONTADOR_SEGUNDOS
+    INC  AUX
+    STS  CONTADOR_SEGUNDOS, AUX
+
+    ; -----------------------------------------
+    ; Verifica se passaram 2 segundos
+    ; -----------------------------------------
+
+    CPI  AUX, PERIODO_DHT
+    BRNE FIM_ISR
+
+    ; -----------------------------------------
+    ; Zera contador
+    ; -----------------------------------------
+
+    CLR  AUX
+    STS  CONTADOR_SEGUNDOS, AUX
+
+    ; -----------------------------------------
+    ; Levanta FLAG para leitura do DHT11
+    ; -----------------------------------------
+
+    LDI  AUX, 1
+    STS  FLAG_DHT, AUX
+
+FIM_ISR:
+
+    ; -----------------------------------------
+    ; Restaura contexto
+    ; -----------------------------------------
+
+    POP  AUX
+    OUT  SREG, AUX
+
+    POP  AUX
+
+    RETI
